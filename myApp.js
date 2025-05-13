fbd6266d873b330457d1ff518c9169a39dc2633b
chadi <csalama@uoc.edu>
2025-05-13 14:10:04 +0200

    fix express.static4

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 // Definir la ruta para el camino raíz '/'
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(path.join(__dirname, '/views/index.html'));
   });
 
 module.exports = app; // Exportar la app para que sea utilizada en server.js
